3c6e1bde891c9c81162858b87a7bc805a831d8af
Ali Kemal Tamkoc <atamkoc@gmail.com>
2025-12-18 16:05:55 +0300

    Initial Commit

---
+++ Deneme2PowerConsumption/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Deneme2PowerConsumption
+//
+//  Created by Ali Kemal on 18.12.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Deneme2PowerConsumption/Deneme2PowerConsumptionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Deneme2PowerConsumptionApp.swift
+//  Deneme2PowerConsumption
+//
+//  Created by Ali Kemal on 18.12.25.
+//
+
+import SwiftUI
+
+@main
+struct Deneme2PowerConsumptionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
